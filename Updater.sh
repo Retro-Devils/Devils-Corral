b4bfb90d2793766ef7459730dc47b5a2b600acdd
Retro-Devils <88673522+Retro-Devils@users.noreply.github.com>
2023-06-27 03:27:45 +0100

    Update Updater.sh

--- Updater.sh
+++ Updater.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
 
+if [ -f "$HOME/.corral/V-1.2" ]; then
+dialog  --sleep 1 --title "UPDATE INFO" --msgbox " 
+YOU ARE UP TO DATE. 
+CUREENT VERSION: 1.2" 0 0
+  else
+dialog  --sleep 1 --title " ERROR!!" --msgbox " 
+THIS IS FOR DEVILS CORRAL ONLY!!!!!" 0 0
+fi
+}
